ba8f718d6ce17e033333a8e460cb97c36323db84
UigeoMun <ugmun@deepbio.co.kr>
2019-01-16 10:05:45 +0900

    add datepicker

---
+++ js/datetimePicker.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('#datetimepicker1').datetimepicker({
+        format : 'YYYY-MM-DDTHH:mm'
+    });
+    $('#datetimepicker2').datetimepicker({
+        format : 'YYYY-MM-DDTHH:mm'
+    });
+});
